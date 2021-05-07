bece46ca92fc321045b97e6aeff8a7a6c32f569d
Lyle Kilson <lylekilsongithub@gmail.com>
2021-05-06 20:36:33 -0600

    bug fixes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,13 @@
 // api routes middleware goes into this file to be exported to controllers/index.js
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-const ownerRoutes = require('./owner-routes.js');
-const vehicleRoutes = require('./vehicle-routes.js');
-const maintRoutes = require('./maintenance-routes.js');
+const ownerRoutes = require("./owner-routes");
+const vehicleRoutes = require("./vehicle-routes");
+const maintRoutes = require("./maintenance-routes");
 
+router.use("/owners", ownerRoutes);
+router.use("/vehicle", vehicleRoutes);
+router.use("/maintlogs", maintRoutes);
 
-;
-router.use('/owners', ownerRoutes);
-router.use('/vehicle', vehicleRoutes);
-router.use('/maintlogs', maintRoutes );
-
-module.exports= router;
+module.exports = router;
--- controllers/api/maintenance-routes.js
+++ controllers/api/maintenance-routes.js
@@ -1,55 +1,60 @@
 // routes for maintenance log?
 
-const router = require('express').Router();
-const {Owner, Vehicle, MaintLog} = require('../../models');
+const router = require("express").Router();
+const { Owner, Vehicle, MaintLog } = require("../../models");
 
-router.get('/' ,(req, res) => {
- MaintLog.findAll({
-     attributes: ['id','last_oil_change', 'last_tire_rotation', 'last_spark_plugs', 'last_air_filter', 'notes', 'vehicle_id']
- })
- .then(dbMaintData => res.json(dbMaintData))
- .catch(err => {
-     console.log(err);
-     res.status(500).json(err)
-  });
+router.get("/", (req, res) => {
+  MaintLog.findAll({
+    attributes: [
+      "id",
+      "last_oil_change",
+      "last_tire_rotation",
+      "last_spark_plugs",
+      "last_air_filter",
+      "notes",
+      "vehicle_id",
+    ],
+  })
+    .then((dbMaintData) => res.json(dbMaintData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-
-
-router.post('/', (req, res) => {
-    MaintLog.create({
-        last_oil_change: req.body.last_oil_change,
-        last_tire_rotation: req.body.last_tire_rotation,
-        last_spark_plugs: req.body.last_spark_plugs,
-        last_air_filter: req.body.last_air_filter,
-        notes: req.body.notes,
-        vehicle_id: req.body.vehicle_id
-    })
-    .then(newMaintLog => res.json(newMaintLog))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    })
+router.post("/", (req, res) => {
+  MaintLog.create({
+    last_oil_change: req.body.last_oil_change,
+    last_tire_rotation: req.body.last_tire_rotation,
+    last_spark_plugs: req.body.last_spark_plugs,
+    last_air_filter: req.body.last_air_filter,
+    notes: req.body.notes,
+    vehicle_id: req.body.vehicle_id,
+  })
+    .then((newMaintLog) => res.json(newMaintLog))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.delete('/:id',  (req, res) => {
-    MaintLog.destroy({
-        where: {
-            id: req.params.id 
-        }
+router.delete("/:id", (req, res) => {
+  MaintLog.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbMaintData) => {
+      if (!dbMaintData) {
+        res.status(404).json({ message: "no log with that id" });
+        return;
+      }
+      res.json(dbMaintData);
     })
-    .then(dbMaintData => {
-        if (!dbMaintData) {
-            res.status(404).json({message: 'no log with that id'})
-            return;
-        }
-        res.json(dbMaintData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-})
-
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
